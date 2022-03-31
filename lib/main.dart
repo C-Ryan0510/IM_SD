b8d4d8f05db0fcf2713776c009391ad04cbc0693
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-03-31 15:35:16 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '健康看護',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
