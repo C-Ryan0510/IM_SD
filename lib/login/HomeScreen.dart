f32b868d448669192c5eb9eb51bb3881eb3aeca1
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-03-31 13:05:57 +0800

    Add files via upload
    edit

---
+++ lib/login/HomeScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState ();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  // TODO: implement widget
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      appBar: AppBar(
+
+      ),
+    )
+  }
+}
---
+++ lib/login/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: Center(
+          child: Text('Login Form', style: TextStyle(fontSize: 40),),
+        )
+    );
+  }
+}
