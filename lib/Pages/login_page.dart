09edbebb64ddc7a48297c5eb79db940b056d01f8
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-04-14 16:08:27 +0800

    Update login_page.dart

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -72,9 +72,9 @@ class LoginPage extends StatelessWidget {
                     TextButton(
                       onPressed: (() {
                         // Navigator.pushNamed(
-                          //   context,
-                          //   MyRoutes.signUp,
-                          // );
+                        //   context,
+                        //   MyRoutes.signUp,
+                        // );
                       }),
                       child: sign_up,
                     ),
@@ -98,86 +98,5 @@ class LoginStateful extends StatefulWidget {
   State<LoginStateful> createState() => _MyStatefulWidgetState();
 }
 class _MyStatefulWidgetState extends State<LoginStateful> {
-  late BuildContext context;
-
-  //show massage by Bar
-  void _showSnackBar(String msg) {
-    final snackBar = SnackBar(
-      content: Text(msg),
-      duration: Duration(seconds: 3),
-      backgroundColor: Colors.white,
-    );
-    Scaffold.of(context).showSnackBar(snackBar);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    this.context = context;
-    //TextField Editer
-    final nameController = TextEditingController();
-    //用戶資料(username,password)填寫的Textfield()
-    final userField = TextField(
-      controller: nameController,
-      style: TextStyle(fontSize: 30),
-      decoration: InputDecoration(
-        labelText: '帳號',
-        labelStyle: TextStyle(fontSize: 30),
-      ),
-    );
-    final passWordField = TextField(
-      controller: nameController,
-      style: TextStyle(fontSize: 30),
-      decoration: InputDecoration(
-        labelText: '密碼',
-        labelStyle: TextStyle(fontSize: 30),
-      ),
-    );
-    //登入button
-    final btn = RaisedButton(
-      child: Text('登入'),
-      /*onPressed()是button必需的*/
-      onPressed: () => _showSnackBar(nameController.text),// for test
-    );
-    //創建帳號/*need push to registerPage()*/
-    final sign_up = Text('創建帳號',
-      style: TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.bold,
-        color: Colors.blueAccent,
-      ),
-    );
-
-    return Scaffold(/*改排版 image maybe?*/
-      body: SafeArea(//no text in Edge of phone
-        minimum: const EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-        child: Padding(//set position
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Container(child: userField, width: 200, margin: EdgeInsets.symmetric(vertical: 10),),
-                Container(child: passWordField, width: 200, margin: EdgeInsets.symmetric(vertical: 10),),
-                Container(child: btn, margin: EdgeInsets.symmetric(vertical: 10),),
-                Row(//sign up
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Don\'t have an account?'),
-                    TextButton(
-                      onPressed: (() {//no registerPage yet
-                        // Navigator.pushNamed(
-                        //   context,
-                        //   MyRoutes.signUp,
-                        // );
-                      }),
-                      child: sign_up,
-                    ),
-                  ],
-                )
-              ]
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+//   ...
 }
