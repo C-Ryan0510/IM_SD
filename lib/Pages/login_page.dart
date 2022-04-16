949245368a6f0bb587e7b2ec52c81cfdf2d5c2be
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-04-16 14:27:20 +0800

    Update login_page.dart

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -24,22 +24,22 @@ class LoginPage extends StatelessWidget {
     var PassWordController = TextEditingController();
     //用戶資料(username,password)填寫的Textfield()
     final userField = TextField(
-        controller: nameController,
-        style: TextStyle(fontSize: 30),
-        decoration: InputDecoration(
-          labelText: '帳號',
-          labelStyle: TextStyle(fontSize: 30),
-        ),
-      );
+      controller: nameController,
+      style: TextStyle(fontSize: 30),
+      decoration: InputDecoration(
+        labelText: '帳號',
+        labelStyle: TextStyle(fontSize: 30),
+      ),
+    );
     final passWordField = TextField(
-        controller: PassWordController,//diffent
-        style: TextStyle(fontSize: 30),
-        decoration: InputDecoration(
-          labelText: '密碼',
-          labelStyle: TextStyle(fontSize: 30),
-        ),
-      );
-    //登入button
+      controller: PassWordController,//diffent
+      style: TextStyle(fontSize: 30),
+      decoration: InputDecoration(
+        labelText: '密碼',
+        labelStyle: TextStyle(fontSize: 30),
+      ),
+    );
+    //登入button test
     final btn = ElevatedButton(
       child: Text('登入'),
       /*onPressed()是button必需的*/
@@ -56,31 +56,43 @@ class LoginPage extends StatelessWidget {
 
     return Scaffold(/*改排版 image maybe?*/
       body: SafeArea(//no text in Edge of phone
-        minimum: const EdgeInsets.symmetric(vertical: 200, horizontal: 50),
-        child: Padding(//set position
+        minimum: const EdgeInsets.symmetric(horizontal: 50),//pixel overflow cause by keyboard(if too large)
+        child: Padding(//set position of box
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           child: Center(
             child: Column(
-              children: <Widget>[
-                Container(child: userField, width: 200, margin: EdgeInsets.symmetric(vertical: 10),),
-                Container(child: passWordField, width: 200, margin: EdgeInsets.symmetric(vertical: 10),),
-                Container(child: btn, margin: EdgeInsets.symmetric(vertical: 10),),
-                Row(//sign up
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Don\'t have an account?'),
-                    TextButton(
-                      onPressed: (() {
-                        // Navigator.pushNamed(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[//that box
+                  Container(child: userField, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
+                  Container(child: passWordField, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
+                  Row(//forgot password & sign_in btn
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(
+                        onPressed: (() {//to forgotPassword
+
+                        }),
+                        child: Text('不記得密碼了嗎?', style: TextStyle(fontSize: 18),),
+                      ),
+                      btn,//alignment: Alignment.centerRight, margin: EdgeInsets.symmetric(vertical: 10),
+                    ],
+                  ),
+                  Row(//sign up
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('尚未建立帳戶?'),
+                      TextButton(
+                        onPressed: (() {//to registerPage()
+                          // Navigator.pushNamed(
                           //   context,
                           //   MyRoutes.signUp,
                           // );
-                      }),
-                      child: sign_up,
-                    ),
-                  ],
-                )
-              ]
+                        }),
+                        child: sign_up,
+                      ),
+                    ],
+                  )
+                ]
             ),
           ),
         ),
