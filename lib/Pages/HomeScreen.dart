68a672917d92b1f35885134f8aedf0b8e7f3b09c
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-04-18 01:01:05 +0800

    Update HomeScreen.dart

--- lib/Pages/HomeScreen.dart
+++ lib/Pages/HomeScreen.dart
@@ -13,11 +13,11 @@ class HomeScreen extends StatelessWidget {
 }
 
 //網址： https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html
+//HomePage() ==> HomeStateful
 //custom StatefulWidget class
 class HomeStateful extends StatefulWidget {
   //constructor
   const HomeStateful({Key? key}) : super(key: key);
-
   @override//Must override
   State<HomeStateful> createState() => _MyStatefulWidgetState();
 }
@@ -26,28 +26,28 @@ class _MyStatefulWidgetState extends State<HomeStateful> {
   int _selectedIndex = 0;
   //統一字體Style
   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  //點擊動作
+  //點擊動作(get index of list)
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
-  //List of Widgets for Navigation Bars
+  //List of Widgets when Navigation Bars(clicked)
   static const List<Widget> _widgetOptions = <Widget>[
-    /*改這裡... Text() OR go to other pages*/
+    /*這裡只是Text()改這裡... 加東西 OR go to other pages*/
     Text('0: 首頁',
       style: optionStyle,
     ),
-    Text('1: 分析',
+    Text('1: 數據',
       style: optionStyle,
     ),
-    Text('2: 設定',
+    Text('2: 分析',
       style: optionStyle,
     ),
     /*看要不要再多*/
-    // Text('3: ',
-    //   style: optionStyle,
-    // ),
+    Text('3: 設定',
+      style: optionStyle,
+    ),
   ];
   @override
   Widget build(BuildContext context) {
@@ -59,10 +59,14 @@ class _MyStatefulWidgetState extends State<HomeStateful> {
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        items: const <BottomNavigationBarItem>[/*要有甚麼*/
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: '首頁',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.data_saver_on),
+            label: '數據',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.bar_chart),
@@ -72,7 +76,6 @@ class _MyStatefulWidgetState extends State<HomeStateful> {
             icon: Icon(Icons.settings),
             label: '設定',
           ),
-          /*這裡也要改*/
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
