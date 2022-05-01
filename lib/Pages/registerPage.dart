4bc9ef2a3381918a0d428d3cf24b0866614cfe90
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-05-01 21:38:26 +0800

    5/1
    widgetful state

--- lib/Pages/registerPage.dart
+++ lib/Pages/registerPage.dart
@@ -1,70 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/Pages/login_page.dart';
 
-class registerPage extends StatelessWidget{
+
+class registerPage extends StatefulWidget{
+  const registerPage({Key?key}) : super (key:key);
+  static const String routeName = "/PageLoading";
+  @override
+  State<StatefulWidget> createState() => _registerState();
+}
+
+class _registerState extends State<registerPage>{
+  late TextEditingController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     //base data
     final account = TextField(
-       decoration: InputDecoration(
-         border: OutlineInputBorder(),
-         hintText: '請輸入帳號',
-       ),
+      controller: _controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: '請輸入帳號',
+      ),
     );
     final password = TextField(
+      controller: _controller,
       decoration: InputDecoration(
         border: OutlineInputBorder(),
         hintText: '請輸入密碼',
       ),
     );
     final comfirmbtn = ElevatedButton(//any type of button
-      onPressed: () {
-        // save textfield to personal Data(table)
-        // redirect to loginPage()
-      },
       child: Text('確認', style: TextStyle(fontSize: 20),),
+      onPressed: (() {
+        /*save textfield to personal Data(table)*/
+        // if(account.onSubmitted&&password.onSubmitted){
+        //   Navigator.pop(context);
+        // }else{
+        // }
+        Navigator.pop(context);
+      }),
     );
-
 
     // 老人或照護者
     var typeAcc = RadioListTile(value: null, onChanged: null, groupValue: null,);
 
-
     return Scaffold(
-      appBar: AppBar(//Back Arrow/*maybe use like sidebar @HomeScreen()*/
-        title: const Text('lol'),
-      ),
-      body: SafeArea(//no text in Edge of phone
-        minimum: const EdgeInsets.symmetric(horizontal: 50),//pixel overflow cause by keyboard(if too large)
-        child: Padding(//set position of box
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Center(
-            child: Column(//the box of all
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[//that box
-                  Container(child: account, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
-                  Container(child: password, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children:[
-                      // typeAcc,//can't be see
-                      comfirmbtn,
-                    ],
-                  ),
-                ]
+      body: Stack(
+          children:[
+            IconButton(//show back Arrow/*can apply on every page*/
+              icon: Icon(Icons.arrow_back,color: Colors.black,),
+              onPressed: () => Navigator.pop(context),//
             ),
-          ),
-        ),
+            SafeArea(//no text in Edge of phone
+              minimum: const EdgeInsets.symmetric(horizontal: 50),//pixel overflow cause by keyboard(if too large)
+              child: Padding(//set position of box
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: Center(
+                  child: Column(//the box of all
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[//that box
+                        Container(child: account, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
+                        Container(child: password, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children:[
+                            comfirmbtn,
+                          ],
+                        ),
+                      ]
+                  ),
+                ),
+              ),
+            ),
+          ]
       ),
-    );  }
-
-}
-
-class _PageLoading extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    );
   }
-
 }
