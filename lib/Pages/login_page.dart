cb302d382c20cfc43978202c32194dd37beb5ec9
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-05-02 18:53:51 +0800

    5/2
    新增 signIn()功能[executable]
            toSignUp [error] (routes not exist)
    簡化 var Widget

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/Firebase/authentication.dart';
-import 'package:untitled/Pages/registerPage.dart';
+import 'package:untitled/main.dart';
 
 //where ==>loginPage()
 class loginPage extends StatefulWidget{
@@ -10,67 +10,17 @@ class loginPage extends StatefulWidget{
   State<loginPage> createState() => _login();
 }
 class _login extends State<loginPage> {
-  //TextField Editer
-  var nameController = TextEditingController();
-  var PassWordController = TextEditingController();
+  //用戶資料(username,password)填寫的Textfield()//須放在widget內才能用
+  final nameController = TextEditingController();
+  final PassWordController = TextEditingController();
   @override
   void dispose() {
     nameController.dispose();
     PassWordController.dispose();
     super.dispose();
   }
-  String getEmail(){
-    return nameController.text;
-  }
-  TextEditingController getPassword(){
-    return PassWordController;
-  }
   @override
-  Widget build(BuildContext context) {
-
-    /*用戶資料(username,password)填寫的Textfield()*/
-    final userField = TextField(
-      controller: nameController,
-      style: TextStyle(fontSize: 30),
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: '輸入帳號',
-        labelStyle: TextStyle(fontSize: 20),
-      ),
-    );
-    final passWordField = TextField(
-      controller: PassWordController,//different
-      style: TextStyle(fontSize: 30),
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: '輸入密碼',
-        labelStyle: TextStyle(fontSize: 20),
-      ),
-    );
-    //登入button test
-    final btn = ElevatedButton(
-      child: Text('登入'),
-      /*onPressed()是button必需的*/
-      onPressed: () => null,
-    );
-    //忘記密碼text
-    final forgot  = Text('不記得密碼了嗎?',
-      style: TextStyle(
-        fontSize: 15,
-        fontWeight: FontWeight.bold,
-        color: Colors.blueAccent,
-      ),
-    );
-    //創建帳號text
-    final sign_up = Text('創建帳號',
-      style: TextStyle(
-        fontSize: 12,
-        fontWeight: FontWeight.bold,
-        color: Colors.blueAccent,
-      ),
-    );
-
-    return Scaffold(/*改再排版(image or animations)*/
+  Widget build(BuildContext context) => Scaffold(/*改再排版(image or animations)*/
       body: SafeArea(//no text in Edge of phone
         minimum: const EdgeInsets.symmetric(horizontal: 50),//pixel overflow cause by keyboard(if too large)
         child: Padding(//set position of box
@@ -79,18 +29,52 @@ class _login extends State<loginPage> {
             child: Column(//the box of all
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[//that box
-                  Container(child: userField, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
-                  Container(child: passWordField, alignment: Alignment.center, margin: EdgeInsets.symmetric(vertical: 10)),
+                  Container(//name or email box
+                      child: TextField(
+                        controller: nameController,
+                        style: TextStyle(fontSize: 20),
+                        textInputAction: TextInputAction.next,//next textfield
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: '輸入帳號',
+                          labelStyle: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.symmetric(vertical: 10)
+                  ),
+                  Container(
+                      child: TextField(
+                          controller: PassWordController,
+                          style: TextStyle(fontSize: 20),
+                          textInputAction: TextInputAction.done,//deselect textfield
+                          obscureText: true,//hide PassWord
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: '輸入密碼',
+                            labelStyle: TextStyle(fontSize: 25),
+                          )
+                      ),
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.symmetric(vertical: 10)
+                  ),
                   Row(//forgot password & sign_in btn
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       TextButton(
-                        onPressed: (() {//to forgotPassword()
-
-                        }),
-                        child: forgot,
+                        child: Text('不記得密碼了嗎?',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent,
+                          ),
+                        ),//to Forget()
+                        onPressed: (){},
                       ),
-                      btn,//alignment: Alignment.centerRight, margin: EdgeInsets.symmetric(vertical: 10),
+                      ElevatedButton(
+                          child: Text('登入'),
+                          onPressed: signIn,/*2@main.dart return HomeScreen()!*/
+                      ),
                     ],
                   ),
                   Row(//sign up
@@ -98,10 +82,14 @@ class _login extends State<loginPage> {
                     children: [
                       const Text('尚未建立帳戶?'),
                       TextButton(
-                        onPressed: (() {//to registerPage()
-                          Navigator.pushNamed(context, '/register',);
-                        }),
-                        child: sign_up,
+                        child: Text('創建帳號',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                        onPressed: () => Navigator.of(context).pushNamed('/register'),
                       ),
                     ],
                   ),
@@ -110,6 +98,23 @@ class _login extends State<loginPage> {
           ),
         ),
       ),
+  );
+
+  Future signIn() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(child: CircularProgressIndicator(),)
     );
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: nameController.text.trim(),
+        password: PassWordController.text.trim(),
+      );
+    } on FirebaseAuthException catch(e) {
+      print(e);
+    }
+    //Hide showDialog
+    navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
 }
