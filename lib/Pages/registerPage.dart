49c7d827d6f85ae2bf3334680728e7bec05420a4
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-05-05 14:15:03 +0800

    5/5
    修正 鍵盤輸入時破圖
    新增 標題字，Icon

--- lib/Pages/registerPage.dart
+++ lib/Pages/registerPage.dart
@@ -34,73 +34,64 @@ class _registerState extends State<registerPage>{
 
   @override
   Widget build(BuildContext context) =>Scaffold(
-    body: Stack(
-        children:[
-          SafeArea(//no text in Edge of phone
-            minimum: const EdgeInsets.symmetric(horizontal: 50),//pixel overflow cause by keyboard(if too large)
-            child: Padding(//set position of box
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              child: Form(
-                key: formKey,
-                child: Column(//the box of all
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[//that box
-                      Container(child: TextFormField(
-                        controller: mailController,
-                        textInputAction: TextInputAction.next,
-                        decoration: InputDecoration(labelText: '請輸入Email帳號',
-                          border: OutlineInputBorder(),
-                        ),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (email) /*email_validator needed add in pubspec.yaml*/
-                        => email != null&& !EmailValidator.validate(email)? '請輸入正確的mail':null,
-                      ),
-                          alignment: Alignment.center,
-                          margin: EdgeInsets.symmetric(vertical: 10)
-                      ),
-                      Container(child: TextFormField(
-                        controller: passwordController,
-                        textInputAction: TextInputAction.next,
-                        obscureText: true,
-                        decoration: InputDecoration(labelText: '請設立密碼',
-                          border: OutlineInputBorder(),
-                        ),
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: (value) => value != null && value.length < 6
-                        ? '請設立長度最少為6的密碼' : null,
-                      ),
-                          alignment: Alignment.center,
-                          margin: EdgeInsets.symmetric(vertical: 10)
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children:[
-                          ElevatedButton(//any type of button
-                            child: Text('創建帳戶', style: TextStyle(fontSize: 20),),
-                            onPressed: SigUp,
-                          )
-                        ],
-                      ),
-                      Container(
-                        child: Row(//sign up
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text('已經有帳戶了?',style: TextStyle(fontSize: 12,color: Colors.black),),
-                              GestureDetector(
-                                onTap: () { widget.togglePage();},//Switch to loginPage()
-                                child: Text('返回登入畫面',
-                                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent,),
-                                ),
-                              ),
-                            ]
-                        ),
-                      ),
-                    ]
-                ),
-              ),
+    resizeToAvoidBottomInset: false,
+    backgroundColor: Colors.deepPurpleAccent,
+    body: Center(
+      child: ListView(
+        shrinkWrap: true,
+        reverse: true,
+        padding: EdgeInsets.all(32),
+        children: [
+          Image.asset('lib/asset/healthcare.png',scale: 2,),
+          Positioned(child: Text('創建新帳號',style: TextStyle(fontSize: 30),),),
+          SizedBox(height: 16,),
+          TextFormField(
+            controller: mailController,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(labelText: '請輸入Email帳號',
+              border: OutlineInputBorder(),
             ),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (email) /*email_validator needed add in pubspec.yaml*/
+            => email != null&& !EmailValidator.validate(email)? '請輸入正確的mail':null,
           ),
-        ]
+          SizedBox(height: 16,),
+          TextFormField(
+            controller: passwordController,
+            textInputAction: TextInputAction.next,
+            obscureText: true,
+            decoration: InputDecoration(labelText: '請設立密碼',
+              border: OutlineInputBorder(),
+            ),
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) => value != null && value.length < 6
+                ? '請設立長度最少為6的密碼' : null,
+          ),
+          SizedBox(height: 16,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              ElevatedButton(//any type of button
+                child: Text('創建帳戶', style: TextStyle(fontSize: 20),),
+                onPressed: SigUp,
+              )
+            ],
+          ),
+          SizedBox(height: 10,),
+          Row(//sign up
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('已經有帳戶了?',style: TextStyle(fontSize: 12,color: Colors.black),),
+                GestureDetector(
+                  onTap: () { widget.togglePage();},//Switch to loginPage()
+                  child: Text('返回登入畫面',
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blueAccent,),
+                  ),
+                ),
+              ]
+          ),
+        ].reversed.toList(),
+      ),
     ),
   );
   Future SigUp() async {
