7220af283f35c0ca1badad58a0ade15058a1be72
蔣 <55950100+C-Ryan0510@users.noreply.github.com>
2022-05-12 18:19:30 +0800

    Update HomeScreen.dart

--- lib/Pages/HomeScreen.dart
+++ lib/Pages/HomeScreen.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:page_indicator/page_indicator.dart';
+import 'package:untitled/homePageModel/healthScreen.dart';
+import 'package:untitled/homePageModel/mainHomeScreen.dart';
+import 'package:untitled/homePageModel/recordScreen.dart';
 
 //main()==>HomeScreen()
 class HomeScreen extends StatefulWidget {
@@ -15,8 +17,19 @@ class _HomeStateful extends State<HomeScreen> {
   // int _selectedIndexBN = 0;
   int _selectedIndexBN = 1;//注:此數字不可存在於超出(Screens、NavigationBar)最大數-1[like index of Array]
   // int _selectedIndexBN = 2;
-  final _BannerController = PageController();
+
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  greeting() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Morning';
+    }
+    if (hour < 17) {
+      return 'Afternoon';
+    }
+    return 'Evening';
+  }
 
   @override
   void initState() {
@@ -25,140 +38,12 @@ class _HomeStateful extends State<HomeScreen> {
 
   //Bodys(配合底部導覽進行轉換)
   final Screens = [ /*Normal widget or Pages()*/
-    // ForgotPage(),// test
     /*記錄*/
-    Padding(
-      padding: EdgeInsets.all(45),
-      child: GridView(
-        gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,//3格寬太小
-          mainAxisSpacing: 20,crossAxisSpacing: 20,
-        ),
-        children: [/*(image,text,[route?])*/
-          //View menuItem() @ bottom of this file
-          _menuItem(imagePath: 'lib/asset/health_Pills.jpg', ItemData: '藥品',),
-          _menuItem(imagePath: 'lib/asset/diet_period.jpg', ItemData: '每日飲食',),
-          _menuItem(imagePath: 'lib/asset/health_Heart_pulse.jpg', ItemData: '生命跡象',),
-          _menuItem(imagePath: 'lib/asset/diet_water.jpg', ItemData: '水分攝取',),
-          _menuItem(imagePath: 'lib/asset/health_special.jpg', ItemData: '!?!?!?!',),
-          _menuItem(imagePath: 'lib/asset/diet_daily.jpg', ItemData: '?????',),
-          _menuItem(imagePath: 'lib/asset/diet_calories.jpg', ItemData: '熱量',),
-          _menuItem(imagePath: 'lib/asset/diet_special.jpg', ItemData: '健康養分',),
-          _menuItem(imagePath: 'lib/asset/healthcare.png', ItemData: 'Coming Soon!',),
-          /*add more by the number of functions*/
-        ],
-      ),
-    ),
+    RecordScreen(),
     /*主頁*/
-    Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        Stack(
-          children: [
-            Positioned(child: Text('最新消息',style: TextStyle(fontSize: 24),)),
-            Container(
-              height: 300,
-              width: double.infinity,
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.2)
-              ),
-              child: PageIndicatorContainer(//import '//page_indicator.dart';
-                indicatorSelectorColor: Colors.blueGrey,
-                length: 4,//needed/*Change*/
-                child : PageView(//ListView滑動時會滑到物件中間&also don't have indicator
-                  scrollDirection: Axis.horizontal,
-                  children: [/*(text,image,[Path?/Url?])*/
-                    _newsItems(text: 'one', imagePath: 'lib/asset/Coming_Soon.jpg'),
-                    _newsItems(text: 'two', imagePath: 'lib/asset/Coming_Soon.jpg'),
-                    _newsItems(text: 'three', imagePath: 'lib/asset/Coming_Soon.jpg'),
-                    _newsItems(text: 'Coming Soon', imagePath: 'lib/asset/Coming_Soon.jpg'),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 5,),//
-        Stack(
-          children: [
-            Positioned(child: Text('最新消息',style: TextStyle(fontSize: 24),)),
-            Container(
-              height: 400,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                // border: BorderSide(color: Colors.white,width: 10),
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.purpleAccent.withOpacity(0.3),
-              ),
-              padding: EdgeInsets.only(top: 40,left: 15,right: 15),
-              child: ListView(
-                children: [
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                  SizedBox(height: 10,),
-                  Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ]
-        ),
-      ],
-    ),
-
+    mainHome(),
     /*健康*/
-    Center(child: Text('HEALTH I don\'t care!!',style: TextStyle(fontSize: 30),),),
+    HealthScreen(),
   ];
 
   @override
@@ -166,6 +51,7 @@ class _HomeStateful extends State<HomeScreen> {
     final user = FirebaseAuth.instance.currentUser!;//當前登入帳戶for info
     return Scaffold(
       backgroundColor: Color.fromRGBO(120, 60, 40, 60),
+      // backgroundColor: Colors.white,//test
       key: _scaffoldKey,
       body: SafeArea(
         child: Stack(
@@ -209,7 +95,7 @@ class _HomeStateful extends State<HomeScreen> {
                   ),
                 ),
                 child: Container(
-                  color: Colors.white,
+                  color: Colors.white.withOpacity(0.7),
                   child: Column(
                     children: [
                       Text('Sign In As', style: TextStyle(fontSize: 20),),
@@ -289,72 +175,6 @@ class _HomeStateful extends State<HomeScreen> {
               label: '健康',
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class _newsItems extends StatelessWidget{
-  final String text;
-  final String imagePath;
-  // final String url;
-  _newsItems({required this.text, required this.imagePath, /*required this.url*/});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.5),
-      ),
-      padding: EdgeInsets.all(15),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Center(child: Image.asset('$imagePath',fit: BoxFit.contain,),),
-          Positioned(child: Text('$text',style: TextStyle(fontSize: 24),),bottom: 5,left: 10,),
-        ],
-      ),
-    );
-  }
-}
-
-//記錄方式
-class _menuItem extends StatelessWidget {
-  final String imagePath;
-  final String ItemData;
-  // route
-  _menuItem({required this.imagePath, required this.ItemData});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        // color: Colors.transparent,
-        color: Colors.white,
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            // side: BorderSide(width: 5,color: Colors.grey),
-          ),
-          onPressed: () {},//navigator-->otherPage
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('$imagePath',),
-              Text(
-                '$ItemData',
-                style: TextStyle(
-                    backgroundColor: Colors.white,
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                ),
-              )
-            ],
-          ),
         ),
       ),
     );
